df567e19af3813c21ac5ba5e57108263fa3933fd
bakinam <btaessawy@gmail.com>
2020-07-02 11:18:14 -0400

    add read raster, shapefile functions

--- scripts/python/open_source_scripts/functions.py
+++ scripts/python/open_source_scripts/functions.py
@@ -1,7 +1,28 @@
 import geopandas as gpd
+import rasterio
+from skimage import io
+import fiona
 
-def load_file(arg):
+def load_shp_file(arg):
     output = gpd.read_file(arg[1])
     return arg[0], output
 
 
+def load_gdb_shp_file(arg):
+    output = gpd.read_file(arg[1], driver=arg[2], layer=arg[3])
+    return arg[0], output
+
+def load_tif_file(arg):
+    output = io.imread(arg[1])
+    return arg[0], output
+
+def records(shapefile):
+    with fiona.open(shapefile, ignore_fields = ['VINTAGE']) as source:
+        for feature in source:
+                yield feature
+         
+        
+def shp_file_with_null(arg): 
+    output = gpd.GeoDataFrame.from_features(records(arg[1]))
+    return arg[0], output
+    
